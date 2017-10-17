ddc0742905cc1a285a0bc78f24ea8f382fefafa6
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-17 17:24:53 +0200

    Click and remove added

--- onclick.js
+++ onclick.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     var message = $('<span>This must be inserted after we click Guess it</span>');
+    var messageBlank = $('<span>asaa</span>');
     $('button').on('click', function() {
-    $('#resultDaily').append(message);
+        $('#resultDaily').append(message);
+    });
+    $('#resultDaily').on('click', function() {
+        $('#resultDaily > span').remove();
     });
 });
 
